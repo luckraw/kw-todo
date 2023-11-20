d0c2d4d17ac83e6159e480a6ed537876e3907ef0
nulld <krawdev@gmail.com>
2023-11-20 19:13:41 -0300

    Initial commit

---
+++ src/main/java/com/luckraw/kwtodo/KwTodoApplication.java
@@ -0,0 +1,13 @@
+package com.luckraw.kwtodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KwTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KwTodoApplication.class, args);
+	}
+
+}
