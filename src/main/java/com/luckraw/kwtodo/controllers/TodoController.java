4666ea42d99979d1ae0422a633c640a29baa1923
nulld <krawdev@gmail.com>
2023-11-20 22:20:21 -0300

    feat: controller routes, list/forms att.

--- src/main/java/com/luckraw/kwtodo/controllers/TodoController.java
+++ src/main/java/com/luckraw/kwtodo/controllers/TodoController.java
@@ -1,8 +1,10 @@
 package com.luckraw.kwtodo.controllers;
 
+import com.luckraw.kwtodo.models.Todo;
 import com.luckraw.kwtodo.repositories.TodoRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Map;
@@ -19,5 +21,16 @@ public class TodoController {
     @GetMapping("/")
     public ModelAndView list() {
         return new ModelAndView("todo/list", Map.of("todos", todoRepository.findAll()));
+    }
+
+    @GetMapping("/create")
+    public ModelAndView create() {
+        return new ModelAndView("todo/form", Map.of("todo", new Todo()));
+    }
+
+    @PostMapping("/create")
+    public String create(Todo todo) {
+        todoRepository.save(todo);
+        return "redirect:/";
     }
 }
